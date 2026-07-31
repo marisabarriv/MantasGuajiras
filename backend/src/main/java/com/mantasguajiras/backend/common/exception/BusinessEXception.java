56eb0d54e13f9e085843ceb39d8acd2c1a605238
marisabarriv <mariaisabelbarrosrivera@gmail.com>
2026-07-31 01:22:45 -0500

    feat(product): implement CRUD operations and exception handling

--- backend/src/main/java/com/mantasguajiras/backend/common/exception/BusinessEXception.java
+++ backend/src/main/java/com/mantasguajiras/backend/common/exception/BusinessEXception.java
@@ -1,5 +1,5 @@
 package com.mantasguajiras.backend.common.exception;
 
-public class BusinessEXception {
+public class BusinessException {
     
 }
--- backend/src/main/java/com/mantasguajiras/backend/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/mantasguajiras/backend/common/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.mantasguajiras.backend.common.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -21,5 +22,18 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Ha ocurrido un error interno.");
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidation(MethodArgumentNotValidException ex) {
+
+        String message = ex.getBindingResult()
+            .getFieldErrors()
+            .get(0)
+            .getDefaultMessage();
+
+        return ResponseEntity
+            .status(HttpStatus.BAD_REQUEST)
+            .body(message);
+}
 
 }
